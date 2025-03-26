492755ddfe447f405d7126a9c0fe8c0fb1e8b445
moses-25 <moses.ochieng1@student.moringaschool.com>
2025-03-26 12:05:14 +0300

    implement character fetching from local API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-// Your code here
+// java script findex.html 
+
+const rendercharacters = async () => {
+let url = "http://localhost:3000/characters"
+
+const response = await fetch(url);
+const characters = await response.json();
+console.log(characters);
+
+}
+
